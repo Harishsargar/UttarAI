71e1aa17c5912c60193142d00f882390a0b6a2de
Harish Sargar <harishsargar19@gmail.com>
2025-06-15 23:26:08 +0530

    woorking on register form

--- aiWriterFrontend/src/components/auth/RegisterForm.jsx
+++ aiWriterFrontend/src/components/auth/RegisterForm.jsx
@@ -1,27 +1,28 @@
-
+import '../../style/registerform.css'
 
 function RegisterForm(){
     return(
-        <div>
-            <form>
+        <div className='form-container'>
+            <form className='register-form'>
                 <label>Enter Name</label>
                 <input type="text" required/>
                 <label>Enter email</label>
-                <input type="email"/>
+                <input type="email" required/>
                 <label>Enter phone number</label>
-                <input type="number"/>
+                <input type="number" required/>
                 <label>Choose gender</label>
                 <div>
-                    <input type="radio" name="gender" value="male"/>
+                    <input type="radio" name="gender" value="male" required/>
                     <label>Male</label>
-                    <input type="radio" name="gender" value="female"/>
+                    <input type="radio" name="gender" value="female" required/>
                     <label>Female</label>
                 </div>
                 
                 <label>Enter password</label>
-                <input type="password"/>
+                <input type="password" required/>
                 <label>Confirm password</label>
-                <input type="password"/>
+                <input type="password" required/>
+                <button type='submit'>Register</button>
             </form>
         </div>
     )
