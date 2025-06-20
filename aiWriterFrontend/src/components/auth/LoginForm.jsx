2d0a26ee45a31847aa72a9a742e29db8f16639e7
Harish Sargar <harishsargar19@gmail.com>
2025-06-20 23:18:35 +0530

    completed login  frontend logic

--- aiWriterFrontend/src/components/auth/LoginForm.jsx
+++ aiWriterFrontend/src/components/auth/LoginForm.jsx
@@ -1,18 +1,46 @@
-import '../../style/loginform.css'
+import "../../style/loginform.css";
+import { useState } from "react";
+import { loginUser } from "../../api/auth";
 
-function LoginForm(){
+function LoginForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-return(
-    <div className='form-container'>
-        <form className='login-form'>
-            <label>Enter Email </label>
-            <input type="email" required/>
-            <label>Enter Password </label>
-            <input type="password" required/>
-            <button type="submit">submit</button>
-        </form>
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    try {
+        await loginUser(formData);
+        setMessage("login Successfull");        
+    } catch (err) {
+        console.log(err)
+        setMessage("Login Failed");
+    }
+  }
+
+  return (
+    <div className="form-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label>Enter Email </label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange}  required />
+        <label>Enter Password </label>
+        <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+        <button type="submit">submit</button>
+        {message && <p>{message}</p>}
+      </form>
     </div>
-)
+  );
 }
 
 export default LoginForm;
