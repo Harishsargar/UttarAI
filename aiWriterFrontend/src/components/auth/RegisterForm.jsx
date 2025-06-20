7a3f90e94fd9dd69fb6318401c073480df10837c
Harish Sargar <harishsargar19@gmail.com>
2025-06-20 23:08:31 +0530

    completed registraion frontend logic

--- aiWriterFrontend/src/components/auth/RegisterForm.jsx
+++ aiWriterFrontend/src/components/auth/RegisterForm.jsx
@@ -1,55 +1,111 @@
-    import '../../style/registerform.css'
-    import { registerUser } from '../../api/auth';
-    import { useState } from 'react';
+import "../../style/registerform.css";
+import { registerUser } from "../../api/auth";
+import { useState } from "react";
 
-    function RegisterForm(){
-        const [formData, setFormData] = useState({
-            name:"",
-            email:"",
-            phoneNumber:"",
-            gender:"",
-            password:"",
-            confirmData:""
-        })
+function RegisterForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    gender: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-        const handleChange =(e)=>{
-            const {name, value} = e.target;
-                setFormData((prev)=>({
-                    ...prev,
-                    [name]:value,
-                }));
-            
-        }
+  const [message, setMessage] = useState('');
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-
-
-
-        return(
-            <div>
-                <form className='register-form'>
-                    <label>Enter Name</label>
-                    <input type="text" value={formData.name} required/>
-                    <label>Enter email</label>
-                    <input type="email" value={formData.email} required/>
-                    <label>Enter phone number</label>
-                    <input type="number" value={formData.phoneNumber} required/>
-                    <label>Choose gender</label>
-                    <div>
-                        <input type="radio" name="gender" value="male" checked={formData.gender=="male"} required/>
-                        <label>Male</label>
-                        <input type="radio" name="gender" value="female" checked={formData.gender=="female"} required/>
-                        <label>Female</label>
-                    </div>
-                    
-                    <label>Enter password</label>
-                    <input type="password" name='password' value={formData.password} required/>
-                    <label>Confirm password</label>
-                    <input type="password" name='confirmPassword' value={formData.confirmData} required/>
-                    <button type='submit'>Register</button>
-                </form>
-            </div>
-        )
-
+  const handleSubmit= async (e)=>{
+    e.preventDefault();
+    if(formData.password != formData.confirmPassword){
+        setMessage("Password did not match !!");
+        return;
     }
-    export default RegisterForm;
+    try{
+        await registerUser(formData);
+        setMessage('Registraion Successfull');
+    }catch(err){
+        console.log(err);
+        setMessage("Registraion Failed");
+    }
+  }
+
+  return (
+    <div>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <label>Enter Name</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <label>Enter email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <label>Enter phone number</label>
+        <input
+          type="number"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+          required
+        />
+        <label>Choose gender</label>
+        <div>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={formData.gender == "male"}
+            onChange={handleChange}
+            required
+          />
+          <label>Male</label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={formData.gender == "female"}
+            onChange={handleChange}
+            required
+          />
+          <label>Female</label>
+        </div>
+
+        <label>Enter password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <label>Confirm password</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Register</button>
+        {message && <p>{message}</p>}
+      </form>
+    </div>
+  );
+}
+export default RegisterForm;
