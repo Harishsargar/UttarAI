c71e81e6fd6d44d124a7d320177421cb46c01371
Harish Sargar <harishsargar19@gmail.com>
2025-06-22 12:31:07 +0530

    implemented AuthenticationProvider and PasswordEncoder

---
+++ aiwriter/src/main/java/com/aireplye/aiwriter/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.aireplye.aiwriter.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.aireplye.aiwriter.service.CustomUserDetailService;
+
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
