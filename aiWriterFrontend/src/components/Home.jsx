64636a838f48218d935e03817cf9e8d7b13889e3
Harish Sargar <harishsargar19@gmail.com>
2025-06-24 23:37:08 +0530

    implemented logout and fixes home page

--- aiWriterFrontend/src/components/Home.jsx
+++ aiWriterFrontend/src/components/Home.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { useNavigate,Link } from "react-router-dom";
 
+
 function Home(){
+    const [message, setMessage]= useState("");
+
+    const handleLogout = () =>{
+        localStorage.removeItem('token');
+        setMessage("User logout successfully !!");
+    }
 
     return(
         <>
             <h1>HOME PAGE</h1>
+            <button onClick={handleLogout}>Logout</button><br/>
+            {message && <p>{message}</p>}
+            <br/><br/>
             <Link to={'/login'}><button>Login</button></Link>
-        
+            <br/><br/><br/>
+            <Link to={'/emailReplyer'}><button>Email Replyer</button></Link>
         </>
     )
 }
