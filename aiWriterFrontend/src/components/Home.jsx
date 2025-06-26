905e938371164444d01a1dce6fa109df7050923a
Harish Sargar <harishsargar19@gmail.com>
2025-06-26 23:44:22 +0530

    make changes in homepage

--- aiWriterFrontend/src/components/Home.jsx
+++ aiWriterFrontend/src/components/Home.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
+function Home() {
+  const [message, setMessage] = useState("");
 
-function Home(){
-    const [message, setMessage]= useState("");
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setMessage("User logout successfully !!");
+  };
 
-    const handleLogout = () =>{
-        localStorage.removeItem('token');
-        setMessage("User logout successfully !!");
-    }
-
-    return(
-        <>
-            <h1>HOME PAGE</h1>
-            <button onClick={handleLogout}>Logout</button><br/>
-            {message && <p>{message}</p>}
-            <br/><br/>
-            <Link to={'/login'}><button>Login</button></Link>
-            <br/><br/><br/>
-            <Link to={'/emailReplyer'}><button>Email Replyer</button></Link>
-        </>
-    )
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+      <button onClick={handleLogout}>Logout</button>
+      <br />
+      {message && <p>{message}</p>}
+      <br />
+      <br />
+      <Link to={"/login"}>
+        <button>Login</button>
+      </Link>
+      <br />
+      <br />
+      <br />
+      <Link to={"/emailReplyer"}>
+        <button>Email Replyer</button>
+      </Link>
+    </>
+  );
 }
 
 export default Home;
