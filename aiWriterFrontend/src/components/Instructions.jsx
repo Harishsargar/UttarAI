7f5fe66e0de2bc7546adc451792ad7813521b2e5
Harish Sargar <harishsargar19@gmail.com>
2025-07-10 18:22:32 +0530

    extension zip for download

--- aiWriterFrontend/src/components/Instructions.jsx
+++ aiWriterFrontend/src/components/Instructions.jsx
@@ -9,7 +9,8 @@ function Instructions() {
                     <div className="download inst ">
                         <div className="instruction-step">1</div>
                         <div className="inst-desc">
-                            <div className="inst-button"><a className="inst-extension"><div>Get Extension</div></a>
+                            <div className="inst-button"><a className="inst-extension" href="/uttar-ai-extension.zip"
+      download><div>Get Extension</div></a>
                             </div>
                             <p> Download the Uttar-AI Extension and <span>unzip</span> it </p>
                             <p>( currently unavailable on chrome store as it is paid )</p>
