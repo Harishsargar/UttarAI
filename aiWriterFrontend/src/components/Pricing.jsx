bd1c997768ea0e7a813c71c889610970d4d18198
Harish Sargar <harishsargar19@gmail.com>
2025-07-15 17:41:05 +0530

    razorpay integrated in frontend

--- aiWriterFrontend/src/components/Pricing.jsx
+++ aiWriterFrontend/src/components/Pricing.jsx
@@ -34,10 +34,39 @@ const Pricing = () => {
         try {
             const order  = "Basic Plan"
             const response = await createOrder({order})
+
+            var options = {
+            "key": "rzp_test_re44xLAcI84GUD", // Enter the Key ID generated from the Dashboard
+            "amount": "50000", // Amount is in currency subunits. 
+            "currency": "INR",
+            "name": "Acme Corp", //your business name
+            "description": "Test Transaction",
+            "image": "https://example.com/your_logo",
+            "order_id": response.data.id, // This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+            "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
+            "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
+                "name": "Gaurav Kumar", //your customer's name
+                "email": "gaurav.kumar@example.com",
+                "contact": "+919876543210" //Provide the customer's phone number for better conversion rates 
+            },
+            "notes": {
+                "address": "Razorpay Corporate Office"
+            },
+            "theme": {
+                "color": "#3399cc"
+            }
+        };
+        var rzp1 = new Razorpay(options);
+        rzp1.open();
+
+
         } catch (error) {
             console.log(error)
             alert("something went wrong");
         }
+
+
+        
     }
 
     return (
